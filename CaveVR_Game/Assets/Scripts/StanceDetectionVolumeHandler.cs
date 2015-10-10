8e78bf8dbd35818c833d4d2a06154ba8853face2
DomCristaldi <domeniccristaldi@gmail.com>
2015-10-10 10:37:18 -0400

    ++ Stance Detection Volumes are set and and managed via the Stance Handler

---
+++ CaveVR_Game/Assets/Scripts/StanceDetectionVolumeHandler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+public class StanceDetectionVolumeHandler : MonoBehaviour {
+
+    public enum Side {
+        Left,
+        Right,
+    }
+
+    public enum StanceType {
+        Roof,
+        Ox,
+        Plow,
+        Fool
+    }
+
+    public BoxCollider col;
+
+    public Side thisSide;
+    public StanceType thisStanceType;
+
+
+
+    void Awake() {
+        col = GetComponent<BoxCollider>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public bool CheckContains(Vector3 pos) {
+        return col.bounds.Contains(pos);
+    }
+}
---
+++ CaveVR_Game/Assets/Scripts/StanceHandler.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class StanceHandler : MonoBehaviour {
+
+    [System.Serializable]
+    public class StanceInfo {
+        public StanceDetectionVolumeHandler dectectVolume;
+        public bool isInVolume = false;
+
+        public StanceInfo() {
+
+        }
+
+        public bool CheckIsInVolume(Vector3 pos) {
+            isInVolume = dectectVolume.CheckContains(pos);
+            return isInVolume;
+        }
+    }
+
+    public Transform weaponTf;
+
+    //public List<StanceDetectionVolumeHandler> stanceDetectionVolumes;
+    public List<StanceInfo> stanceInfos;
+
+    public StanceInfo currentStance;
+
+    void OnDisable() {
+        currentStance = null;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateCurrentStance();
+	}
+
+    
+    //UPDATE THE STATE OF ALL STANCES AND ASSIGN THE CURRENT STANCE
+    private void UpdateCurrentStance() {
+        if (stanceInfos == null) { return; }
+
+        foreach (StanceInfo sI in stanceInfos) {
+            if (sI.CheckIsInVolume(weaponTf.position) == true) {
+                currentStance = sI;
+            }
+
+        }
+
+    }
+}

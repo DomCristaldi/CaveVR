a5be6e58b20db948806227cfc416aa51a5d76e45
Ben Chang <changb3@rpi.edu>
2015-10-16 16:07:23 -0400

    .. Modified length of sword ++ added some stuff to the scene

---
+++ CaveVR_Game/Assets/Scripts/Motor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Motor : MonoBehaviour {
+
+	public Vector3 moveDirec;
+
+	public string forwardAxis;
+	public string sideAxis;
+
+	void Awake() {
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
